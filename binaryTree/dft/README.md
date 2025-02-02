755c4b095df8184acd4f7f6d596bb1e017399e82
Poorna <34907975+poorna1995@users.noreply.github.com>
2025-02-02 13:26:11 +0530

    Implement dft code

--- binaryTree/dft/README.md
+++ binaryTree/dft/README.md
@@ -1,3 +1,7 @@
 Depth First Search
 
 ![alt text](image-1.png)
+
+## Algorithm
+
+![alt text](image-2.png)
---
+++ binaryTree/dft/code.py
@@ -0,0 +1,33 @@
+class BSTNode:
+    def __init__(self,key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class DepthFirstTraversal:
+    def dft(self,root):
+        stack = [root]
+        values = []
+        while len(stack)>0:
+            node = stack.pop()
+            values.append(node.key)
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
+        return values
+
+
+
+
+root = BSTNode(10)
+root.left = BSTNode(5)
+root.right = BSTNode(15)
+root.left.left = BSTNode(3)
+root.left.right = BSTNode(7)
+root.right.left = BSTNode(12)
+root.right.right = BSTNode(18)
+
+df = DepthFirstTraversal()
+result = df.dft(root)
+print(result)
