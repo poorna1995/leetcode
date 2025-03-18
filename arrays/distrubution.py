39cc8aa91cb75fee0f9906a969664652b9568aff
Poorna <34907975+poorna1995@users.noreply.github.com>
2025-03-18 14:20:25 +0530

    array

--- arrays/distrubution.py
+++ arrays/distrubution.py
@@ -1,8 +1,29 @@
-def ChocolateDistribution(a):
-    for i in range(len(a)-1):
+# def ChocolateDistribution(a, m):
+#     n = len(a)
+#     a.sort()
+#     minimum = float('inf')
+#     for i in range(n-m+1):
+#         diff = a[i+m-1] -a[i]
+#         if diff < minimum:
+#             minimum = diff
+#     print(minimum)
+#     return minimum
 
-    print(a)
+def ChocolateDistribution(a, m):
+    n = len(a)
+    a.sort() 
+
+    minimum = float('inf')
+
+    for i in range(n - m + 1):
+
+        diff = a[i + m - 1] - a[i]
+        if diff < minimum:
+            minimum = diff
+    return minimum
 
 a = [7, 3, 2, 4, 9, 12, 56]
+m = 3
 
-ChocolateDistribution(a)
+result = ChocolateDistribution(a,m)
+print(result) # Expected output: 2
