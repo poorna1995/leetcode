1d864394978eed27b4ce8ea1a5cfeec2eede3187
Poorna <34907975+poorna1995@users.noreply.github.com>
2025-04-15 11:01:03 +0530

    two_pointers

---
+++ leetcode75/sliding_sindow/long_substring.py
@@ -0,0 +1,39 @@
+# Given a string s, find the length of the longest substring without duplicate characters.
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+
+def LongestSubstring(s):
+    n = len(s)
+    result = ''
+    max_length = 0
+    for str in s:
+        if str not in result:
+            result +=str
+
+    
+
+        
+        
+    
+    
+# s = "abcabcbb"
+# s= "bbbbb"
+s= "pwwkew"
+    
+print(LongestSubstring(s))
